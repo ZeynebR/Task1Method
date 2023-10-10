7be166f9f7895d66e84552dc83d4c85114fb01c6
ZeynebR <zeynabnr@code.edu.az>
2023-10-10 08:22:25 +0200

    method task added

---
+++ Task1Method/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+
+
+int[] Numbers = { 2, 4, 7, 8, 10 };
+
+int NewNumber = 7;
+
+Console.WriteLine(FindNumber(Numbers, NewNumber));
+
+
+bool FindNumber(int[] numbers, int NewNumber)
+{
+    for (int i = 0; i < Numbers.Length;i++)
+    {
+        if (Numbers[i] == NewNumber)
+        {
+            
+            return true;
+        }
+    }
+    return false;
+
+}
+
